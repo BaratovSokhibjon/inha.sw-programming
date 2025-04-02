79a63d6e4bc94091d9db9deb8b19f22f7d620dd3
Komronbek <kamronbektokhirov@gmail.com>
2025-04-02 22:01:14 +0900

    Removed duplicate files, keeping them inside the folder

--- parse_json.py
+++
@@ -1,8 +0,0 @@
-import json
-
-try:
-    with open('myfile_12235649.json') as f:
-        data = json.load(f)
-    print(f"Access token: {data['access_token']}\nExpires in: {data['expires_in']} sec")
-except (FileNotFoundError, json.JSONDecodeError, KeyError) as e:
-    print(f"Error: {str(e)}")
--- parse_yaml.py
+++
@@ -1,9 +0,0 @@
-import yaml
-
-with open('myfile_12235649.yaml', 'r') as file:
-    parsed_data = yaml.safe_load(file) 
-
-print("The access token is {}".format(parsed_data['access_token']))
-print("The token expires in {} seconds.".format(parsed_data['expires_in']))
-print("The refresh token is {}".format(parsed_data['refresh_token']))
-print("The refresh token expires in {} seconds.".format(parsed_data['refreshtokenexpires_in']))
